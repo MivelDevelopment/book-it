6f4438e53aa7b888a4101222a7426d5ede5c6576
Milos Veljkovic <66674475+milosveljkovic1992@users.noreply.github.com>
2022-04-11 14:19:14 +0200

    Improved single appointment tab on dashboard

--- src/components/appointment-info/index.js
+++ src/components/appointment-info/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {
-    Container
+    Container, Heading, Info
 } from './styles/appointment-info';
 
 
 const AppointmentInfo = ({ children, ...restProps }) => {
     return <Container {...restProps}>{children}</Container>
 }
+
+AppointmentInfo.Heading = ({ children, ...restProps }) => {
+    return <Heading {...restProps}>{children}</Heading>
+}
+
+AppointmentInfo.Info = ({ children, ...restProps }) => {
+    return <Info {...restProps}>{children}</Info>
+}
+
 
 export default AppointmentInfo;
--- src/containers/dashboard/appointment-list/single-appointment-details.js
+++ src/containers/dashboard/appointment-list/single-appointment-details.js
@@ -6,17 +6,17 @@ export const SingleAppointmentDetails = ({ booking }) => {
     
     
     return (
-        <AppointmentInfo>
+        <>
         {booking && 
         <>
-            <h3>Scheduler information</h3>
-            <p>Name: <b>{booking?.scheduler?.fullName}</b></p>
-            <p>Email: <b>{booking?.scheduler?.email}</b></p>
-            <p>Phone number: <b>{booking?.scheduler?.number}</b></p>
-            <p>Message: <b>{booking?.scheduler?.message}</b></p>
-            <h3>Scheduled</h3>
-            <p><b>{booking?.appointment?.day} / {booking?.appointment?.month} / {booking?.appointment?.year}</b> at <b>{booking?.appointment?.time}</b></p>
+            <AppointmentInfo.Heading>Scheduler information</AppointmentInfo.Heading>
+            <AppointmentInfo.Info>Name: <b>{booking?.scheduler?.fullName}</b></AppointmentInfo.Info>
+            <AppointmentInfo.Info>Email: <b>{booking?.scheduler?.email}</b></AppointmentInfo.Info>
+            <AppointmentInfo.Info>Phone number: <b>{booking?.scheduler?.number || 'N/A'}</b></AppointmentInfo.Info>
+            <AppointmentInfo.Info>Message: <b>{booking?.scheduler?.message || 'N/A'}</b></AppointmentInfo.Info>
+            <AppointmentInfo.Heading>Scheduled</AppointmentInfo.Heading>
+            <AppointmentInfo.Info><b>{booking?.appointment?.day < 10 ? `0${booking?.appointment?.day}` : booking?.appointment?.day} / {booking?.appointment?.month + 1 < 10 ? `0${booking?.appointment?.month + 1}` : booking?.appointment?.month + 1} / {booking?.appointment?.year}</b> at <b>{booking?.appointment?.time}</b></AppointmentInfo.Info>
         </>}
-        </AppointmentInfo>
+        </>
     )
 }
