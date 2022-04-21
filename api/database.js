be9cee130194cb7bc4ed6579a6738a11d3a408ae
Milos Veljkovic <66674475+milosveljkovic1992@users.noreply.github.com>
2022-04-21 08:39:02 +0200

    Created backend for user auth

---
+++ api/database.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        email: "johndoe@gmail.com",
+        password: 'sickofeveryoneusingmyname'
+    },
+    {
+        email: "janedoe@gmail.com",
+        password: 'chrisrockwenttoofar'
+    },
+    {
+        email: "jl@gmail.com",
+        password: 'cmonnowsingwmeyall'
+    },
+    {
+        email: "luger88@gmail.com",
+        password: 'forthelasttimeimnotyourfather'
+    },
+
+]
