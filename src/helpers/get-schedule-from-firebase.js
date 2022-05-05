b299ed7606d19da62004b7d16ce00a31f34cb4c2
Milos Veljkovic <66674475+milosveljkovic1992@users.noreply.github.com>
2022-05-05 10:29:49 +0200

    Changed directory

--- src/helpers/get-schedule-from-firebase.js
+++
@@ -1,9 +0,0 @@
-import { db } from '../firebase-config';
-import { doc, getDoc } from 'firebase/firestore';
-
-export const getScheduleFromFirebase = async (userEmail = 'jdp@gmail.com') => {
-    const scheduleRef = doc(db, `schedule`, userEmail);
-    const res = await getDoc(scheduleRef);
-    console.log(res.data().appointments);
-    return res.data().appointments;
-}
