dbc029141772bdbed4bf5ade2e65f36ced885dde
Milos Veljkovic <66674475+milosveljkovic1992@users.noreply.github.com>
2022-05-08 00:05:43 +0200

    Restructured dashboard to re-fetch less times

---
+++ src/pages/dashboard.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Dashboard } from '../components';
+import { AppointmentListContainer } from '../containers/dashboard/appointment-list/appointment-list-container';
+import { Btn } from '../components';
+
+
+export const DashboardPage = ({ currentAppointmentShown, setCurrentAppointmentShown, isAppointmentOpen, setIsAppointmentOpen }) => {
+
+    return (
+        <>
+            <Routes>
+                <Route exact path="/" element={
+                    <Dashboard.Column>
+                        <Dashboard.InnerContents>
+                            <h2>Your schedule</h2>
+                            <AppointmentListContainer
+                                currentAppointmentShown={currentAppointmentShown}
+                                setCurrentAppointmentShown={setCurrentAppointmentShown}
+                                isAppointmentOpen={isAppointmentOpen}
+                                setIsAppointmentOpen={setIsAppointmentOpen}
+                            />
+                        </Dashboard.InnerContents>
+                    </Dashboard.Column>}
+                />
+
+                <Route exact path="/:appointmentId" element={
+                    <Dashboard.Column>
+                        <Dashboard.InnerContents>
+                            <h2>Your schedule</h2>
+                            <AppointmentListContainer
+                                currentAppointmentShown={currentAppointmentShown}
+                                setCurrentAppointmentShown={setCurrentAppointmentShown}
+                                isAppointmentOpen={isAppointmentOpen}
+                                setIsAppointmentOpen={setIsAppointmentOpen}
+                            />
+                        </Dashboard.InnerContents>
+                    </Dashboard.Column>}
+                />
+
+            </Routes>
+
+            <Routes>
+                <Route exact path="/" element={
+                    <Dashboard.Column>
+                        <Dashboard.InnerContents>
+                            <h2>Set your availability</h2>
+                            <Btn style={{ marginBottom: 20 }}>
+                                Set one by one
+                            </Btn>
+                            <Btn>
+                                Set multiple at once
+                            </Btn>
+                        </Dashboard.InnerContents>
+                    </Dashboard.Column>}
+                />
+
+
+            </Routes>
+        </>
+    )
+}
--- src/pages/dashboard/single-appointment-page.js
+++ src/pages/dashboard/single-appointment-page.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Dashboard } from '../../components';
 import { SingleAppointmentDetailsContainer } from '../../containers/dashboard/appointment-list/single-appointment-details-container';
-import { AppointmentListContainer } from '../../containers/dashboard/appointment-list/appointment-list-container';
 
-export const SingleAppointmentPage = ({ currentAppointmentShown, setCurrentAppointmentShown, isAppointmentOpen, setIsAppointmentOpen, handleBack }) => {
+export const SingleAppointmentPage = ({ currentAppointmentShown, setIsAppointmentOpen, handleBack }) => {
 
     return (
         <>
-            <Dashboard.Column>
-                <Dashboard.InnerContents>
-                    <h2>Your schedule</h2>
-                    <AppointmentListContainer
-                        currentAppointmentShown={currentAppointmentShown}
-                        setCurrentAppointmentShown={setCurrentAppointmentShown}
-                        isAppointmentOpen={isAppointmentOpen}
-                        setIsAppointmentOpen={setIsAppointmentOpen}
-                    />
-                </Dashboard.InnerContents>
-            </Dashboard.Column>
             <Dashboard.Column>
                 <Dashboard.InnerContents>
                     {currentAppointmentShown.status === 'available'
